a19b421847b50f6d0b482f5427fb414ddea4fb5d
Kencho5 <giokenchadze@gmail.com>
2025-04-13 10:54:33 +0400

    revert: zonejs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
@@ -7,7 +10,7 @@ import { requestInterceptor } from "@core/interceptors/request.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection(),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([requestInterceptor])),
   ],
