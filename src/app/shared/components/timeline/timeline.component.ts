1456c91b43dc5263f09085cfabb55c7778506712
Kencho5 <giokenchadze@gmail.com>
2025-04-29 08:47:58 +0400

    add: numbers to timeline

--- src/app/shared/components/timeline/timeline.component.ts
+++ src/app/shared/components/timeline/timeline.component.ts
@@ -1,8 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { Program } from "@core/interfaces/tv";
 
 @Component({
   selector: "app-timeline",
   imports: [],
   templateUrl: "./timeline.component.html",
 })
-export class TimelineComponent {}
+export class TimelineComponent {
+  @Input() programs!: Program[];
+
+  convertTimestampToHours(timestamp: number): string {
+    const date = new Date(timestamp * 1000);
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    return `${hours}:${minutes}`;
+  }
+}
