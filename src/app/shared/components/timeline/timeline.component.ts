18ef3e8b7f2a13da6bbdeb6c9c08319534584622
Kencho5 <giokenchadze@gmail.com>
2025-04-30 13:20:06 +0400

    update: timeline design

--- src/app/shared/components/timeline/timeline.component.ts
+++ src/app/shared/components/timeline/timeline.component.ts
@@ -1,18 +1,35 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, signal } from "@angular/core";
 import { Program } from "@core/interfaces/tv";
+import { SharedModule } from "@shared/shared.module";
 
 @Component({
   selector: "app-timeline",
-  imports: [],
+  imports: [SharedModule],
   templateUrl: "./timeline.component.html",
 })
 export class TimelineComponent {
   @Input() programs!: Program[];
 
-  convertTimestampToHours(timestamp: number): string {
-    const date = new Date(timestamp * 1000);
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-    return `${hours}:${minutes}`;
+  tooltipX = 0;
+  tooltipText = "";
+  tooltipShown = signal(false);
+
+  hover(event: MouseEvent) {
+    const el = event.currentTarget as HTMLElement;
+    const rect = el.getBoundingClientRect();
+    const relativeX = event.clientX - rect.left;
+    const percent = relativeX / rect.width;
+
+    const totalMinutes = 23 * 60;
+    const minutes = Math.floor(percent * totalMinutes);
+    const hours = Math.floor(minutes / 60)
+      .toString()
+      .padStart(2, "0");
+
+    const mins = (minutes % 60).toString().padStart(2, "0");
+
+    this.tooltipX = relativeX;
+    this.tooltipText = `${hours}:${mins}`;
+    this.tooltipShown.set(true);
   }
 }
