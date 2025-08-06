f6f0fae163191345e842a286edaeaa88e83e681c
Kencho5 <giokenchadze@gmail.com>
2025-08-06 09:52:14 +0400

    fix: language not persistant after refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { CommonModule, ViewportScroller } from "@angular/common";
 import { SharedModule } from "@shared/shared.module";
 import { NavigationEnd, Router } from "@angular/router";
+import { LanguageService } from "@core/services/language.service";
 
 @Component({
   selector: "app-root",
@@ -12,7 +13,10 @@ export class AppComponent {
   constructor(
     private viewportScroller: ViewportScroller,
     private router: Router,
-  ) {}
+    private languageService: LanguageService,
+  ) {
+    this.languageService.setLanguage(this.languageService.currentLang);
+  }
 
   ngOnInit() {
     this.router.events.subscribe((event) => {
